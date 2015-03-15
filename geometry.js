79fc93d0a5d0ff9093f2a6dfc2d4a7e53865c8c1
Gary <gary@dhcp014.mpipks-dresden.mpg.de>
2015-03-15 13:16:20 +0100

    some more vector operations

--- geometry.js
+++ geometry.js
@@ -12,3 +12,12 @@ var normalize = function (vector) {
 var vectorOrientation = function (vector) {
     return Math.atan2(vector[1], vector[0]);
 };
+
+
+var vectorAdd = R.zipWith(R.add);
+
+var vectorNegate = R.map(function (v) { return -v; });
+
+var vectorSub = function (v1, v2) {
+    return vectorAdd(v1, vectorNegate(v2));
+}
